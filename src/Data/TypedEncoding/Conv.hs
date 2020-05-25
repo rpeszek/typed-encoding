d17117e8508709bad1326980e1ead33b65125614
Robert Peszek <peszeks@gmail.com>
2020-05-25 14:16:31 -0600

    docs

--- src/Data/TypedEncoding/Conv.hs
+++ src/Data/TypedEncoding/Conv.hs
@@ -79,7 +79,7 @@
 -- ETE.encodeUtf8 :: (Superset "r-UTF8" r)        => Enc '[r] c T.Text -> Enc '[r] c B8.ByteString  
 -- @
 --
--- @"r-UNICODE.D76"@ and @"r-UTF8"@ is considered redundant for @T.Text@ and can be added and dropped as needed.
+-- @"r-UNICODE.D76"@ and @"r-UTF8"@ is considered redundant for @T.Text@ and can be added or dropped as needed.
 -- 
 -- Corresponding pairs reverse, this should be clear since the types are restricted to what @T.Text@ can store or to 
 -- how @B8.Char@ works.
@@ -92,11 +92,12 @@
 -- EB8.pack :: (Superset "r-CHAR8" r) => Enc '[r] c String -> Enc '[r] c B8.ByteString
 -- @  
 --
--- So what is the set of common values allowing us to use any of these 2 options?
--- "r-UNICODE.D76" is not important here, what is the intersection of /UTF8/ and /CHAR8/ code point space?
+-- What is the set of common values allowing us to use any of these 2 options?
+--
+-- "r-UNICODE.D76" is not important here (it removes a range of Unicode values way above @\'\255\'@), what is the intersection of /UTF8/ and /CHAR8/ code point space?
 --
 -- There are many character set encodings that utilize one byte (/CHAR8/) and /UTF8/ is different from all of them
--- but it backward compatible with the /ASCII/ range of chars @ < 127@.  So the intersection should be /ASCII/, let us check that:
+-- but it backward compatible only within the /ASCII/ range of chars @ < 127@.  So the intersection should be /ASCII/, let us check that:
 --
 -- @
 -- ghci> :t ETE.encodeUtf8 . ET.pack @'["r-ASCII"]
@@ -113,15 +114,34 @@
 -- Now we could run a property test but it is clear that by the design these will match!
 --    
 -- Note, there is no @Superset "r-UNICODE.D76" "r-CHAR8"@ mapping, "r-CHAR8" supersets any
--- 8-bit encoding like /ISO/IEC 8859/ family of encodings.  This effectively prevents these bytes
--- to end up being encoded in @Text@.  This is just one example of added type level security that 
--- /type-encoding/ provides.
+-- 8-bit encoding like /ISO/IEC 8859/ family of encodings.  This is by design even if structurally such 
+-- definition would made sense.
 --
--- Currently, "r-CHAR8" is intended as upper bound on "r-" encodings. There is no way
--- to encode to it via standard encoding mechanism. However, it @Char@ is often used instead of @Word8@
--- for low level @ByteString@ programming. This is supported using @"r-ByteRep"@ annotation which 
--- can be used as @Superset "r-CHAR8" "r-ByteRep"@, this allows for @EncB8@ conversions to work.
--- Again, there is no @Superset "r-UNICODE.D76" "r-ByteRep"@ so these cannot be converted to @Text@.
+-- This choice effectively prevents anything classified under @"r-CHAR8"@
+-- to end up as a visible encoding annotation in @Text@ (since that would made little sense as @Text@ is /UTF/ encoded).  
+-- This is just one example of added type level security that /type-encoding/ provides.
+--
+-- Currently, "r-CHAR8" is intended as upper bound on "r-" encodings only. There is no way
+-- to encode to it using provided encoding mechanisms (except for /unsafe/ options).
+-- Effectively the types
+-- 
+-- @
+-- Enc "r-CHAR8" c str
+-- @
+--
+-- can be viewed as uninhabited. 
+--
+-- However, @Char@ is often used instead of @Word8@
+-- for low level @ByteString@ programming. This is supported with the @"r-ByteRep"@ annotation.
+--
+-- @
+-- Enc "r-ByteRep" c str
+-- @
+--
+-- this one can be used as @Superset "r-CHAR8" "r-ByteRep"@! 
+-- That allows for @EncB8@ conversions to work on such data.
+-- However, there is no @Superset "r-UNICODE.D76" "r-ByteRep"@ so these cannot be converted to @Text@, which is 
+-- exactly what is intended. 
 --
 --
 -- == @Enc@ conversions 
@@ -131,17 +151,17 @@
 -- One challenge is how do we know that @xs@ is a valid encoding stack also for @str2@?
 -- Should we constrain that?      
 --
--- This is made even more difficult because this library plays (has to) games with orphan instances.
+-- This is made even more difficult because this library uses (has to) orphan instances.
 --
--- The other challenge is how to ensure that if the destination decides to partially or fully decode, then
--- it will do so without errors and the decoding will be meaningful.
+-- The other challenge is how to ensure that, if the destination is partially or fully decoded, then
+-- it will decode without errors and the decoding will be meaningful.
 --
 -- Current version does not impose any instance constraint about existence of the stack for @str2@. 
 -- It is possible to not have one, in that case @decodeAll@ combinators will not be available.
 --
 -- This is still useful as the payload could be safely extracted, to save to the database or do other things with it.
 --
--- It is quite possible to try to enforce validity of the stack for @str2@, this is currently not implemented.
+-- Future versions of /typed-encoding/ may provide ways to ensure validity of the encoding stack for @str2@.
 
 
 module Data.TypedEncoding.Conv where
